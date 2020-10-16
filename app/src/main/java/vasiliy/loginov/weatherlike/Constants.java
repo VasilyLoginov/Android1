ed0ee98ffc8e7d4059e44ba4d878ed9c7641bb95
vvloginov <vloginov83@gmail.com>
2020-10-16 03:08:48 +0300

    Lesson 4

---
+++ app/src/main/java/vasiliy/loginov/weatherlike/Constants.java
@@ -0,0 +1,7 @@
+package vasiliy.loginov.weatherlike;
+
+public interface Constants {
+    String TEMP_TEXT = "TEMP_TEXT";
+    String CITY_TEXT = "CITY_TEXT";
+    String ADDR_TEXT = "ADDR_TEXT";
+}
